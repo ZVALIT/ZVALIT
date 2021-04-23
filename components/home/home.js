5154a1906f5e3e7eacf38172fd443d9381612c18
Kiran <kiran@zvalsolutions.com>
2021-04-24 00:23:33 +0530

    #merge master resolve conflicts

--- components/home/home.js
+++ components/home/home.js
@@ -23,4 +23,4 @@ const styles = StyleSheet.create({
         position: 'relative',
         height: '100%'
     }
-})
+});
--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -1,78 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
+import SigninRequest from '../signinRequest/signinRequest';
 import Footer from '../Footer/footer';
-import { FontAwesome, AntDesign } from '@expo/vector-icons';
 
-export default function Portfolio(props){
-    return(
-        <View style={style.Portfolio}>
-            <View style={style.PortfolioHeader}>
-                <Text style={style.PortfolioText}>PORTFOLIO</Text>
-                <View style={style.Icons}>
-                    <FontAwesome name= 'bell' size={24} color= 'orange' />
-                    <AntDesign name= 'plus' size={24} color= 'black' />
-                </View>
-            </View>
-            <View style={style.LogoAndText}>
-                <Image style={style.CoinmomoLogo} source={require( '../../assets/img/logo.png' )} />
-                <Text>Sign in to view your profile</Text> 
-            </View>
-            <View style={style.signupAndSignin}>
-                <TouchableOpacity>
-                    <Text style={style.signinButton}>SIGN IN</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={style.SignupText}>Don't have an account?</Text>
-                </TouchableOpacity>
-            </View>
-            <Footer props={ props }/>   
+export default function Portfolio(props) {
+    return (
+        <View>
+            <SigninRequest />
+            <Footer props={props} />
         </View>
-    )
+    );
 }
-const style = StyleSheet.create({
-    Portfolio:{
-        position: 'relative',
-        height: '100%'
-    },
-    PortfolioHeader:{
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        paddingLeft: 20,
-        paddingRight: 20,
-        marginTop: 25,
-        backgroundColor: 'white',
-        paddingTop: 20,
-        height: 60,
-        borderBottomWidth: 1,
-        borderBottomColor: 'black'
-    },
-    Icons:{
-        width:90,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    signinButton:{
-        height: 40,
-        marginLeft: 25,
-        marginRight: 25,
-        backgroundColor: '#8fc73d',
-        textAlign: 'center',
-        paddingTop: 10,
-        color: 'white',
-    },
-    SignupText:{
-        textAlign: 'center',
-        marginTop: 10
-    },
-    LogoAndText:{
-         marginTop: '25%',
-         alignItems: 'center'
-    },
-    CoinmomoLogo:{
-        width: 150,
-        height: 40
-    },
-    signupAndSignin:{
-        marginTop: '30%'
-    }
-});
